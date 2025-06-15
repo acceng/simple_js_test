2bf45cc8e45443aa1fb4853f44728bbfc9a17745
acceng <52406318+acceng@users.noreply.github.com>
2025-06-15 23:24:26 +0700

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,7 +1,7 @@
 const { add, subtract } = require('./math');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(8);
+  expect(add(1, 2)).toBe(3);
 });
 
 test('subtracts 5 - 3 to equal 2', () => {
